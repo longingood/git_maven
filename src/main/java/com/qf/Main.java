80fd6e74b371d3c0597b5e771dc2cf05127e7927
qf <qf>
2019-05-15 20:55:28 +0800

    修改了一些内容

---
+++ src/main/java/com/qf/Main.java
@@ -0,0 +1,13 @@
+package com.qf;
+
+/**
+ * @version 1.0
+ * @user qf
+ * @date 2019/5/15 20:36
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("你好java");
+        System.out.println("hello github");
+    }
+}
